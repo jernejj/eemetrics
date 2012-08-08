4aed93d510f59a2399db6edd33d874c32b0b692b
matej.crepinsek@gmail.com <matej.crepinsek@gmail.com@ae97b62e-0480-33fe-dbd5-e9fcaa2dc345>
2012-08-08 14:14:12 +0000

    Updated with Sovova DE example.

---  eemetrics/EEAnalysis/src/atree/experimet/realvector/ExperimentSovova.java
+++  eemetrics/EEAnalysis/src/atree/experimet/realvector/ExperimentSovova.java
@@ -11,7 +11,7 @@ import atree.metrics.criteria.EECriteriaEuclidianDistance;
 import atree.metrics.criteria.RevisitedCriteriaEuclidian;
 import atree.treeData.Nodes;
 
-public class ExperimentSimpleTest {
+public class ExperimentSovova {
 	/**
 	 * This is test for real vector input files!
 	 * Reads 3 runs of algorithm and generates one averaged column for latex table.
@@ -25,9 +25,16 @@ public class ExperimentSimpleTest {
 		columsForSumTable.add(PrintStatATMetrics.getDoubleInfoColumn());
 		ArrayList<String> tableHeadsForSumTable = new ArrayList<String>();
 		String tmpSourceFileName;
-		final int MAX_RUNS = 3; //number of repeated
-		String rootPath = "test_cases/realvector/de/";
-		tmpSourceFileName = rootPath + "test_a";
+		final int MAX_RUNS = 30; //number of repeated
+		String rootPath = "test_cases/realvector/de/sovova/";
+		/**
+		 * Popsize = 30; //based on the DE in the paper >Maxgen = 500; //based on
+		 * the DE in the paper >pc (CR) = 0.9 //based on the DE in the paper >pm (F)
+		 * = 0.9 //based on the DE in the paper >Round = 5 (will be increased after
+		 * checking with no bug) >Z = 0~10 >W = 0~10 >x_K = 0~0.0619
+		 */
+		
+		tmpSourceFileName = rootPath + "out_20120808a";
 		String problemExt = ".stat";
 		String fileName;
 		//First Column---------------------------------------------------------
@@ -35,7 +42,7 @@ public class ExperimentSimpleTest {
 		for (int runs = 1; runs <= MAX_RUNS; runs++) {
 			n = new Nodes();
 			fileName = tmpSourceFileName + runs + problemExt;
-			n.createAllFromRealVector(fileName, 500); // read first 500
+			n.createAllFromRealVector(fileName, 501); // read first 500
 			m = new ATMetrics(n.getAllNodes(),
 					new DominantParentCriteriaEuclidianDistanceRealValues(),
 					new EECriteriaEuclidianDistance(1.5),
